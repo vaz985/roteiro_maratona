95c6dde518a8309477a7f2216bab7726fd8d8076
artv <artv@DESKTOP-CP01B0P.localdomain>
2018-02-18 22:32:41 -0300

    wip

--- 2/19.cpp
+++ 2/19.cpp
@@ -1,11 +1,25 @@
 #include<iostream>
 using namespace std;
+//int fun( int s, int * v, int n ) {
+//  int amnt = 0;
+//   
+//  return amnt;
+//}
 int main() {
-  int n, l; 
-  cin >> n >> l;
-  int it = l;
-  while( (it--) > 0 ) {
+  unsigned int n, l; 
+  while( 1 ) {
+    int v[1000001];
+    cin >> n >> l;
 
+    if( n == 0 && l == 0 )
+      return 0;
+
+    for( int i=1; i<=l; i++ )
+      cin >> v[i];
+    
+    for( int i=1; i<=l; i++ ) {
+      int e = v[i] + n - 1;
+    }
   }
   return 0;
 }
