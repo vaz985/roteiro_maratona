3339b3342fd77df9c8ff52265762603da45b8609
artv <artv@debian>
2018-02-20 17:44:31 -0300

    wip

--- 2/20.cpp
+++ 2/20.cpp
@@ -4,10 +4,26 @@ int main() {
   int x[200001];
   int n;
   while( cin >> n ) {
+    int v[n];
+    int amnt = 0;
     for( int i=0; i<n; i++ ) {
       cin >> x[i];
-      //x[i + n] = x[i];
+      x[i + n] = x[i];
+      amnt += x[i];
     }
+    if( amnt == 0 || (amnt % 3 != 0) ) {
+      cout << "0" << endl;
+      continue;
+    }
+    amnt = 0;
+    for(int i=0; i<n; i++) {
+      v[0] = x[i];
+      for( int j=1; j<n; j++ ) {
+       v[j] = v[j-1] + x[i+j];   
+      }
+      
+    }
+    cout << "stop" << endl;
   }
   return 0;
 }
