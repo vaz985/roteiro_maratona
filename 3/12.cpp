8504ad88e182e44538eebcd36047ccf4f5e9813e
artv <artv@debian>
2018-02-26 18:15:36 -0300

    almosttttttt

--- 3/12.cpp
+++ 3/12.cpp
@@ -3,59 +3,60 @@
 #include <vector>
 #include <bitset>
 using namespace std;
-bool comp( bitset<32> a, bitset<32> b ) {
-  if( a.count() >= b.count() )
-    return true;
-  return false;
+struct best_sol {
+  bitset<32> best_s;
+  int d_count;
+};
+struct best_sol bs;
+void f( vector<bitset<32> > v, bitset<32> s, int d, int m ) {
+  if( s.count() >= m && d < bs.d_count ) {
+    bs.best_s = s;
+    bs.d_count = d; 
+  }
+  if( v.size() == 0 ) {
+    return;
+  }
+  else {
+    for( int i=0; i<s.size(); i++ ) {
+      bitset<32> s2 = s | *(v.begin() + i);
+      vector< bitset<32> > v2 = v; v2.erase(v2.begin() + i);
+      f( v2, s2, d+1, m );
+    } 
+  }  
 }
-
 int main() {
   int T; cin >> T;
   while( T-- > 0 ) {
     int m, d;
     cin >> m >> d;
-    list< bitset<32> > l;
+    vector< bitset<32> > v;
     for( int i=0; i<d; i++ ) {
       int n; cin >> n;
-      if( n == 0 ) 
-        continue;
       bitset<32> b;
       for( int j=0; j<n; j++ ) {
         int x; cin >> x;
         b.flip(x);  
       }
-      l.push_back(b);
+      v.push_back(b);
     }
     if( m == 0 ) {
       cout << "0" << endl;
       continue;
     }
-    l.sort(comp);
-    unsigned int count = 1;
-    bitset<32> s = l.front(); l.pop_front();
-    cout << s << endl << endl;
-    while( !l.empty() ) {
-      if( s.count() >= m ) {
-        break;
-      }
-      list< bitset<32> > :: iterator rm;
-      unsigned int max = 0;
-      for( list< bitset<32> > :: iterator it = l.begin(); it != l.end(); it++ ) {
-        if( ((*it).count() -  (s&*it).count()) > max ) {
-          rm = it;
-          max = ((*it).count() - (s&*it).count());
-        }
-      }
-      s |= (*rm);
-      l.erase(rm);
-      count++;
+    unsigned int min = -1;
+    int count;
+    bs.d_count = d+1;
+    for( int i=0; i<d; i++ ) {
+      bitset<32> s = *(v.begin()+i); 
+      vector< bitset<32> > v2 = v; v2.erase(v2.begin()+i);
+      f( v2, s, 1, m );
     }
-    if( s.count() < m ) {
+    if( bs.best_s.count() >= m && bs.d_count <= d ) {
+      cout << bs.d_count << endl;
+    }
+    else {
       cout << "Desastre!" << endl;
-    } else {
-      cout << count << endl;
     }
   }
   return 0;
 }
-// Mudar para uma abordagem gulosa do set cover
