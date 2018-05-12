5badbb19a3fd4a8e1d57b09f70b84f91fb0677a6
Artur Vaz <vaz985@gmail.com>
2018-05-11 21:54:20 -0300

    4

--- 4/01.cpp
+++ 4/01.cpp
@@ -1,21 +1,24 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 int n, m;
 int main() {
   cin >> n >> m; 
-  int last = 0, last_last = 0;
-  int charge = m;
+  vector<ll> p(n+1);
+  for(int i=0; i<n; i++) {
+    cin >> p[i];
+  }
+  p[n] = (ll) 42195;
+  for(ll x : p)
+    cout << x << " ";
+  cout << endl;
+  ll charge = m;
   bool deu = true;
-  cin >> last;
-  for(int i=1; i<n; i++) {
-    last_last = last;
-    cin >> last; 
-    //cout << charge << " " << last - last_last << endl;
-    if(last - last_last <= charge) {
-      charge -= (last-last_last); 
-      charge += m;
+  for(int i=1; i<=n; i++) {
+    if((p[i] - p[i-1]) > charge) {
+      deu = false;
+      break;
     }
-    else deu = false;
   }
   if(deu)
     cout << "S" << endl;
