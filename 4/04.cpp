0b99930724291c6497ab91e7a84636fbb7b39517
artv <artv@DESKTOP-CP01B0P.localdomain>
2018-05-12 23:55:44 -0300

    wip

---
+++ 4/04.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int c, f;
+vector<pair<int,int> > v;
+int main() {
+  int testNo = 1;
+  while(1) {
+    cin >> c >> f;
+    if(c==0) break;
+    if(testNo>1) cout << endl;
+    cout << "Teste " << testNo < <endl;
+    v.assing(f);
+    for(int i=0; i<f; i++) {
+      
+    }
+  } 
+  return 0;
+}
